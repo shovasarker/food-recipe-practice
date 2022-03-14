a47e1a1adbc63b938ccda57c9af671d39a27460a
ShovaSarker <shovasarker01@gmail.com>
2022-03-14 23:53:33 +0600

    removed unused imports

--- src/pages/SearchedPage/index.jsx
+++ src/pages/SearchedPage/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { fetchRecipes } from '../../Api'
 import CustomGrid from '../../components/CustomGrid'
 import Spinner from '../../components/Spinner'
